ef7f3d5cb5c100e5e8e6dcf2356a3cb14958668e
SergioKogut <sergio.kogut@gmail.com>
2019-09-16 17:16:14 +0300

    fix image

--- TouristApp/Controllers/HotelController.cs
+++ TouristApp/Controllers/HotelController.cs
@@ -10,6 +10,12 @@ using TouristApp.ViewModels;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using TouristApp.Helpers;
+using Microsoft.AspNetCore.Hosting;
+using System.IO;
+using System.Drawing;
+using System.Drawing.Imaging;
 
 namespace TouristApp.Controllers
 {
@@ -24,6 +30,8 @@ namespace TouristApp.Controllers
         readonly IUserService _userService;
         readonly IEmailSender _emailSender;
         readonly IFileService _fileService;
+        private readonly IConfiguration _configuration;
+        private readonly IHostingEnvironment _env;
         readonly EFContext _context;
 
         public HotelController(UserManager<DbUser> userManager,
@@ -32,6 +40,9 @@ namespace TouristApp.Controllers
             IFileService fileService,
             IUserService userService,
             IEmailSender emailSender,
+            IHostingEnvironment env,
+            IConfiguration configuration,
+
             EFContext context)
         {
             _userManager = userManager;
@@ -41,6 +52,8 @@ namespace TouristApp.Controllers
             _userService = userService;
             _emailSender = emailSender;
             _roleManager = roleManager;
+            _configuration = configuration;
+            _env = env;
         }
 
         
@@ -100,6 +113,27 @@ namespace TouristApp.Controllers
             return Ok(model);
         }
 
+
+        [HttpGet("images")]
+        public IEnumerable<ImageItemViewModel> Images()
+        {
+            var url = _configuration.GetValue<string>("ImagesUrl");
+            List<string> imageNames = new List<string>
+            {
+                "1", "2", "3", "4"
+            };
+            var model = imageNames
+                .Select(x => new ImageItemViewModel
+                {
+                    Id = int.Parse(x),
+                    BigImage = $"{url}/1200_{x}.jpg",
+                    SmallImage = $"{url}/268_{x}.jpg"
+                }).ToList();
+
+            return model;
+        }
+
+
         // POST: api/hotel/photo
         [HttpPost("photo")]
         public async Task<IActionResult> Post([FromBody] HotelAddPhotoViewModel model)
@@ -109,12 +143,37 @@ namespace TouristApp.Controllers
                 return BadRequest(ModelState);
             }
 
-            string path = _fileService.UploadImage(model.imageBase64);
+            var sizes = _configuration.GetValue<string>("ImagesSizes").Split(' ');
+
+            string imageName = Guid.NewGuid().ToString() + ".jpg";
+            string base64 = model.imageBase64;
+            if (base64.Contains(","))
+            {
+                base64 = base64.Split(',')[1];
+            }
+
+            var bmp = base64.FromBase64StringToBitMap();
+            string fileDestDir = _env.ContentRootPath;
+            fileDestDir = Path.Combine(fileDestDir, _configuration.GetValue<string>("ImagesPath"));
+
+            foreach (var element in sizes)
+            {
+                string fileSave = Path.Combine(fileDestDir, element + "_" + imageName);
+                if (bmp != null)
+                {
+                    int size = Convert.ToInt32(element);
+                    var image = ImageHelper.CompressImage(bmp, size, size);
+                    image.Save(fileSave, ImageFormat.Jpeg);
+                }
+
+            }
+                                   
+            //string path = _fileService.UploadImage(model.imageBase64);
 
             _context.HotelImages.Add(new HotelImages
             {
                 HotelId = model.HotelId,
-                HotelImageUrl = path
+                HotelImageUrl = imageName
             });
 
             await _context.SaveChangesAsync();
--- TouristApp/Startup.cs
+++ TouristApp/Startup.cs
@@ -119,6 +119,13 @@ namespace TouristApp
                 FileProvider = new PhysicalFileProvider(pathRoot),
                 RequestPath = new PathString("/" + Configuration.GetValue<string>("ImagesUrl"))
             });
+
+            app.UseStaticFiles(new StaticFileOptions()
+            {
+                FileProvider = new PhysicalFileProvider(pathRoot),
+                RequestPath = new PathString("/" + Configuration.GetValue<string>("ImagesHotelUrl"))
+            });
+
             #endregion;
 
             app.UseStaticFiles(new StaticFileOptions()
@@ -127,6 +134,7 @@ namespace TouristApp
                 RequestPath = new PathString("/UserImages")
             });
 
+          
 
             app.UseMvc(routes =>
             {
