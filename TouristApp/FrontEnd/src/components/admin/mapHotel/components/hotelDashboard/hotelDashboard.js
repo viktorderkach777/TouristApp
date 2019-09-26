30848491cc2d07b63a45d2ddb6d5d237618e194c
viktorderkach777 <viktorderkach777@yandex.ua>
2019-09-26 17:00:50 +0300

    maplist dashboard start

---
+++ TouristApp/FrontEnd/src/components/admin/mapHotel/components/hotelDashboard/hotelDashboard.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class HotelDashboard extends Component {
+    state = {
+        data: null,
+        active: 0,
+        term: ''
+      };
+      
+    render() {
+        return (
+            <div>Hello, World!</div>
+        );
+    }
+}
+
+export default HotelDashboard;
